0fe4f0424dd359c3ed2abff19e30f114168e8ba9
conny1 <conrad@nathandigital.com>
2024-07-29 23:39:42 +0300

    api ointergration

--- react js/src/App.jsx
+++ react js/src/App.jsx
@@ -1,22 +1,39 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Lines from './components/lines/Lines'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [users, setusers] = useState([])
 
-  const data = [[1], [1,2,3], [1,2,3,4] [1,2,3] , [1,2] , [1,2,3,4,5,6,7] ,[1,2] ]
+
+useEffect(() => {
+  const fetchData = async ()=>{
+    try {
+      const data = await fetch('https://xsrr-l2ye-dpbj.f2.xano.io/api:oUvfVMO5/receive_week?start_date=2024-1-8%20')
+
+      let resp =await data.json()
+      // console.log(resp)
+      setusers(resp)
+    } catch (error) {
+       alert('Error fetching data. Refresh page')
+    }
+  }
+  fetchData()
+ 
+}, [])
+
 
   return (
   
       <div className='homeContainer'>
         <div className='linesContainer' >
         {
-          data.map((i)=>{
-            return < Lines key={i} />
-          })
+          users.length !== 0 ? (
+          users[0]?.array?.map((user, i)=>{
+            return < Lines key={`${user.id}${i}`} />
+          })) : 'Loading ...'
         }
         </div>
        
